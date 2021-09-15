8c9bc28ebb3d8defdfd58591907798b9fe663d83
Emily Howell <emi.howell.37@gmail.com>
2021-09-15 14:46:08 -0400

    initial commit with finalized code
    I wrote the code for this without commits, my apologies if that is against the brief for the project. Adjustment to HTML specifically to change input type "submit" to "button", as well as changing some class or ID names to suit my own preferences. JS code written for the project all before initial commit.

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
 // Select size input
-
-// When size is submitted by the user, call makeGrid()
+const table = document.getElementById('pixelCanvas');
+// Select button input
+const create = document.getElementById('createButton');
 
 function makeGrid() {
-
-// Your code goes here!
-
+    table.innerHTML = ''; //ensures a new column is made each time 'create' is pressed
+    //set variables for rows and columns
+    const width = document.getElementById('inputWidth').value;
+    const height = document.getElementById('inputHeight').value;
+    //create table
+    for(let x = 0; x < height; x++) {
+        const row = document.createElement('tr');
+        table.appendChild(row);
+        for(let y = 0; y < width; y++) {
+            const grid = document.createElement('td');
+            grid.style.backgroundColor = 'white'; //always set the bg color back to white when creating new grid
+            row.appendChild(grid);
+        };
+    };
 }
+
+//function to fill in grid using the event target to target specific grid tiles ("td")
+function draw(event) {
+    const color = document.getElementById('colorPicker').value; // takes color from input, within the function
+    event.target.style.backgroundColor = color;
+}
+
+// When size is submitted by the user, call makeGrid()
+create.addEventListener('click', makeGrid);
+//when table is clicked, call draw(), which uses the event target to specify the grid tile to change
+table.addEventListener('click', draw);
